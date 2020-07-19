0b1083746b4051194f06e6064b9c5c85aa27c3bd
rin1208 <sora1208sky@gmail.com>
2020-07-20 02:47:17 +0900

    commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", hoge)
+	r.Run()
+}
+
+func hoge(c *gin.Context) {
+	c.JSON(200, "hogeeeeeeee")
+}
